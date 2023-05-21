5e7ae7d691e15c7e7ddd83ae54e086d9716b7efd
최혜원 <hwchoi33@naver.com>
2023-05-22 00:10:32 +0900

    urls수정

--- final-pjt-back/urls.py
+++ final-pjt-back/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from movies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('movies/', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
     path('community/', include('community.urls')),
