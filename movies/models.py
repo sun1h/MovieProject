d5a7225d58a8f76850a2e10e31d8e2dbc10ad7a2
최혜원 <hwchoi3355@gmail.com>
2023-05-23 16:27:11 +0900

    추천 알고리즘 로고만들기 db수정

--- movies/models.py
+++ movies/models.py
@@ -6,6 +6,8 @@ from django.conf import settings
 class Genre(models.Model):
     genre_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=50)
+    def __str__(self) :
+        return self.name
 
 
 class Movie(models.Model):
--- movies/urls.py
+++ movies/urls.py
@@ -4,11 +4,11 @@ from movies import views
 app_name = 'movies'
 
 urlpatterns = [
-    path('search/', views.search, name='search'),   # 검색 기능 
-    path('', views.index, name='index'),    # 메인 페이지
-    path('all/', views.all, name='all'),    # 전체 영화
-    path('<int:movie_pk>/', views.detail, name='detail'),   # 개별 영화
+    path('search/', views.search, name='search'),    
+    path('', views.index, name='index'),    
+    path('all/', views.all, name='all'),   
+    path('<int:movie_pk>/', views.detail, name='detail'),   
     path('<int:movie_pk>/like/', views.like, name='like'),
-    path('<int:movie_pk>/<int:rate_pk>/delete/', views.delete_rate, name='delete_rate'),    # 평가 삭제
-    path('recommendation/', views.recommendation, name='recommendation'),    # 영화추천페이지
+    path('<int:movie_pk>/<int:rate_pk>/delete/', views.delete_rate, name='delete_rate'), 
+    path('recommendation/', views.recommendation, name='recommendation'),   
 ]
